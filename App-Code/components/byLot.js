73716b9638cb741112788f83c9071f1834747e79
lhand578 <82662432+lhand578@users.noreply.github.com>
2021-10-20 13:57:50 -0400

    Update byLot.js
    dropdown now functional
    - selects lot and fullness using hardcoded arrays

--- App-Code/components/byLot.js
+++ App-Code/components/byLot.js
@@ -3,11 +3,12 @@ Filename: byLot.js
 Original Author: Lukas H.
 Date of Creation: 7/6/2021
 Description: Page that displays parking lot fullness by lot
-Last Edit: 10/18/2021
+Last Edit: 10/19/2021
 -------------------------------------*/
 
 import React, { Component } from 'react';
-import { View, Picker, Text } from 'react-native';
+import { View, Text } from 'react-native';
+import Picker from 'react-native-universal-picker';
 
 import {
   StyledContainer,
@@ -19,23 +20,22 @@ import {
 
 //will be replaced with database query
 const LotNames = ['Lot A', 'Lot B', 'Lot C'];
-const LotData = ['50%', '100%', '69%'];
-const ViewResult = false;
+const LotFullness = [50, 100, 69];
 
 export default class byLotScreen extends Component {
+  state = {
+    selectedLot: '',
+    selectedLotIndex: 0
+  };
 
-  state = {Lot: ''};  
-
-  //sets the lot selected to what gets displayed on dropdown
-  showLotData =(option)=>{
-    if(option !== 'disabled')
-    {
-      this.setState({Lot: option});      
-    }    
+  onValueChange = (itemValue, itemIndex) => {
+    this.setState({
+      selectedLot: itemValue,
+      selectedLotIndex: itemIndex 
+    });
   }
-  //render the page
+  
   render() {
-
     return (  
       <StyledContainer> 
         {/*header*/}        
@@ -45,23 +45,27 @@ export default class byLotScreen extends Component {
         </View>
 
         {/*dropdown menu (using array listed above)*/}
+        <View style={{dropDownContainer}}>
           <Picker
-              style={{dropDownContainer}}
-              mode="dropdown"
-              selectedValue={this.state.Lot}
-              onValueChange={this.showLotData}> 
-              {LotNames.map((item, index) => {
-                  return (<Picker.Item label={item} value={item} key={index}/>) 
-              })}
-          </Picker>    
+            selectedValue={this.state.selectedLot}
+            onValueChange={this.onValueChange}
+          >        
+        {/*maps array values for LotNames to picker items*/}
+          {LotNames.map((item, index) => {
+              return (
+                <Picker.Item label={item} value={item} key={index}/>) 
+              })}            
+          </Picker>
+        </View>
+
+        {/*display based off what was selected            
+          Conditional code: https://stackoverflow.com/questions/44256969/react-native-display-view-depending-on-conditional
+            if something not selected, display nothing.  if something is selected, list the lot and the associated fullness
+            also uses the index of the selected lot to get the fullness (assumes arrays are of same size)
+        */}
           
-          {/*display based off what was selected
-            //https://www.tabnine.com/code/javascript/classes/react-native/Picker
-            //https://stackoverflow.com/questions/44256969/react-native-display-view-depending-on-conditional
-          */}
+      { this.state.selectedLot == '' ?  <View/> : <View style={{alignItems:'center'}}><Text>{this.state.selectedLot} is selected.  The lot is {LotFullness[this.state.selectedLotIndex]}% full.</Text></View> }
           
-          { this.state.Lot == '' ?  <View/> : <View style={{alignItems:'center'}}><Text>{this.state.Lot} is selected.</Text></View> }
-        
       </StyledContainer>   
     )
   }
