88ac74804c3126828e5dbdd476eb46ac3898f112
lhand578 <82662432+lhand578@users.noreply.github.com>
2021-10-23 12:29:23 -0400

    Update ByLot.js
    - updated code so that it now uses an array of objects in state rather than arrays outside of the class

--- App-Code/components/byLot.js
+++ App-Code/components/byLot.js
@@ -3,7 +3,7 @@ Filename: byLot.js
 Original Author: Lukas H.
 Date of Creation: 7/6/2021
 Description: Page that displays parking lot fullness by lot
-Last Edit: 10/19/2021
+Last Edit: 10/23/2021
 -------------------------------------*/
 
 import React, { Component } from 'react';
@@ -17,24 +17,39 @@ import {
   dropDownContainer
 } from './styles';
 
-
-//will be replaced with database query
-const LotNames = ['Lot A', 'Lot B', 'Lot C'];
-const LotFullness = [50, 100, 69];
-
 export default class byLotScreen extends Component {
-  state = {
-    selectedLot: '',
-    selectedLotIndex: 0
-  };
 
-  onValueChange = (itemValue, itemIndex) => {
-    this.setState({
-      selectedLot: itemValue,
-      selectedLotIndex: itemIndex 
-    });
+
+//sketchy af website but I yoinked the code from here:
+//https://xuykyuu.blogspot.com/2018/07/map-object-to-generate-pickeritem.html
+//much more credible stack overflow reference:
+//https://stackoverflow.com/questions/40201699/objects-in-react-native
+
+constructor() {
+super();
+  this.state = {
+
+    //array of objects containing values
+    LotData : [
+      {
+        "name" : "Lot A",
+        "fullness" : 50,
+      },
+      {
+        "name" : "Lot B",
+        "fullness" : 100,
+      },
+      {
+        "name" : "Lot C",
+        "fullness" : 69,
+      },
+    ],
+    selectedLotName: '',
+    selectedLotFullness: 0,
   }
-  
+}
+
+
   render() {
     return (  
       <StyledContainer> 
@@ -46,16 +61,19 @@ export default class byLotScreen extends Component {
 
         {/*dropdown menu (using array listed above)*/}
         <View style={{dropDownContainer}}>
-          <Picker
-            selectedValue={this.state.selectedLot}
-            onValueChange={this.onValueChange}
-          >        
         {/*maps array values for LotNames to picker items*/}
-          {LotNames.map((item, index) => {
-              return (
-                <Picker.Item label={item} value={item} key={index}/>) 
-              })}            
+        { <Picker 
+            selectedValue={this.state.selectedLotName}
+            onValueChange={(itemValue, itemIndex) => this.setState({selectedLotName:itemValue.name, selectedLotFullness:itemValue.fullness})}
+          >
+
+          {/* maps values to items in the picker.  The value is the object, so when accessing elements you gotta do item.(member you wanna access)*/}
+          {this.state.LotData.map(item => {
+            return(<Picker.Item label={item.name} value={item} key={item.name}/>)
+          })
+          }        
           </Picker>
+        }
         </View>
 
         {/*display based off what was selected            
@@ -63,8 +81,8 @@ export default class byLotScreen extends Component {
             if something not selected, display nothing.  if something is selected, list the lot and the associated fullness
             also uses the index of the selected lot to get the fullness (assumes arrays are of same size)
         */}
-          
-      { this.state.selectedLot == '' ?  <View/> : <View style={{alignItems:'center'}}><Text>{this.state.selectedLot} is selected.  The lot is {LotFullness[this.state.selectedLotIndex]}% full.</Text></View> }
+        
+      {this.state.selectedLotName == '' ?  <View/> : <View style={{alignItems:'center'}}><Text>{this.state.selectedLotName} is selected.  The lot is {this.state.selectedLotFullness}% full.</Text></View>}
           
       </StyledContainer>   
     )
