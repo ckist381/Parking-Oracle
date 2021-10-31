2e0a97500837a9b11b0b783090bada8cdd6d166a
mmose424new <85967957+mmose424new@users.noreply.github.com>
2021-10-31 14:52:55 -0400

    Update MySQLDatabaseScript.py 31Oct2021 Update
    Added new table ByPasses. Changed functions names around these to account for the new change we have with the new tables. Double check all scripts and you might have to change names of functions that you call using my script.

--- MySQLDatabaseScript.py
+++ MySQLDatabaseScript.py
@@ -1,6 +1,6 @@
 # Author:                    Michael Moser
 # Major:                     Computer Science: Information Technology
-# Creation Date:             23Oct2021
+# Creation Date:             09Aug2021 (revision 1 completion date)
 # Course:                    CSC354/355 - 020
 # Professor Name:            Professor DeMarco
 # Filename:                  MySQLDatabaseScript.py
@@ -24,6 +24,13 @@ db = mysql.connector.connect(
 # used to be able to execute statements using mysql library
 mycursor = db.cursor()
 
+# used to create the By Passes Table for the database
+def createByPasses():
+    sql = "CREATE TABLE ByPasses (lotName VARCHAR(255), pass VARCHAR(255));"
+    mycursor.execute(sql)
+    db.commit()
+    print ("inserted new table into database.")
+
 # this is a function that will insert new columns into the database
 # you will need to manually work with this statement
 def alterDatabase():
@@ -32,34 +39,43 @@ def alterDatabase():
     db.commit()
     print (mycursor.rowcount, "inserted new column.")
 
-# this is a function that when called will display the database and all
+# this is a function that when called will display the lot table and all
 # elements when needed
-def showDatabase():
+def showlot():
     sql = "SELECT * FROM lot"
     mycursor.execute(sql)
     records = mycursor.fetchall()
     print(records)
 
-#mycursor.execute("CREATE TABLE lot (name VARCHAR(255) PRIMARY KEY NOT NULL, spacesTaken INT(5), fullPer FLOAT(5,1),\
- #filename VARCHAR(255), url VARCHAR(255))")
-#mycursor.execute("INSERT INTO lot (name, spacesTaken, fullPer, filename, url) VALUES ('testLot', 11, 0, 'testLot.pn\
-#g', 'atesturl.com')")
+# this is a function that when called will show the byPasses table and all
+# elements when needed
+def showByPasses():
+    sql = "SELECT * FROM ByPasses"
+    mycursor.execute(sql)
+    records = mycursor.fetchall()
+    print(records)
 
 # this function will take the name of the lot, the spaces taken in the lot,
 # the fullness percentage of the lot, and the filename of that lot and will
 # create a new record into the database
 def setFullness(lotID, name, spacesTaken, fullPer, filename, url):
-    sql = "INSERT INTO lot (lotID, name, spacesTaken, fullPer, filename, url) VALUES (%s, %s, %s, %s, %s, %s)"
+    sql = "INSERT INTO lot (lotID, name, spacesTaken, fullPer, filename, url) VAL\
+UES (%s, %s, %s, %s, %s, %s)"
     val = (lotID, name, spacesTaken, fullPer, filename, url)
     mycursor.execute(sql, val)
 
     db.commit()
 
-    print (mycursor.rowcount, "record inserted.")
+    print (mycursor.rowcount, "record for lot tabe inserted.")
 
-    #mycursor.execute("SELECT * FROM lot")
-    #for i in mycursor:
-        #print(i)
+# this function will take the name of the lot, and the pass that can park in lot.
+# we have to call it passes for variable as pass is used already in Python.
+def setByPasses(lotName, passes):
+    sql = "INSERT INTO ByPasses (lotName, pass) VALUES (%s, %s)"
+    val = (lotName, passes)
+    mycursor.execute(sql, val)
+    db.commit()
+    print (mycursor.rowcount, "record for ByPasses table inserted.")
 
 # this function will take a name of a lot and the percentage of the spaces full
 # from the vision system and will update the table
@@ -100,11 +116,20 @@ def updateFilename(name, filename):
     # committing (saving) the sql results to database
     db.commit()
 
+# this function will take a name of a lot and the filename of said lot from
+# the vision system and will update the file name (if need be)
+def updateUrls(name, url):
+    sql = "UPDATE lot SET url=%s WHERE name=%s"
+    data = (url, name)
+    mycursor.execute(sql, data)
+    db.commit()
+    pass
+
 # this function will take the name of a parking lot and return a dictionary
 # with the name of the lot as the key and the filename as the value
 def getImageData(name):
     # sql execute statement setting filename based on name
-    sql = "SELECT filename FROM lot WHERE name=%s"
+    sql = "SELECT name, filename FROM lot WHERE name=%s"
     # the parameter passed in above
     data = (name,)
 
@@ -113,9 +138,17 @@ def getImageData(name):
     # printing as a test measure (can be removed)
     mycursor.execute(sql, data)
     fileData = mycursor.fetchall()
+    print (fileData)
 
-    return fileData[0][0]
+    # indexing fileData to retrieve the tuple and extract the values
+    # of the name of the lot Lname and the file location Lfile
+    Lname = fileData[0][0]
+    Lfile = fileData[0][1]
 
+    # combining these two values into a dictionary
+    # returning the dictionary
+    imgDict = {Lname: Lfile}
+    return imgDict
 
 # this function will take the name of a parking lot and return the value of the
 # number of spaces that were taken in that lot.
@@ -129,8 +162,10 @@ def getSpacesTaken(name):
     Ltaken = Ttaken[0][0]
     return (Ltaken)
 
-# this function will return a list of the names of parking lots inside the database inside a list
-# executes the sql statement, fetches all returned items, indexes them puts them into a list and
+# this function will return a list of the names of parking lots inside the 
+# database inside a list
+# executes the sql statement, fetches all returned items, indexes them puts them \
+# into a list and
 # returns the list
 def getNames():
     sql = "SELECT name FROM lot"
@@ -146,63 +181,73 @@ def getNames():
 
     pass
 
-# this function will return a list of the urls of parking lots inside the database inside a list
-# executes the sql statement, fetches all returned items, indexes them puts them into a list and
+# this function will return a list of the urls of parking lots inside the databas\
+e inside a list
+# executes the sql statement, fetches all returned items, indexes them puts them \
+into a list and
 # returns the list
 def getURLs():
-    sql = "SELECT filename, url FROM lot"
+    sql = "SELECT url FROM lot"
     mycursor.execute(sql)
     Lurls = []
     Turls = mycursor.fetchall()
-    print(Turls)
-    return Turls
+    for i in range(len(Turls)):
+        urlTuple = Turls[i]
+        urlItem = urlTuple[0]
+        Lurls.append(urlItem)
+    print (Lurls)
+    return Lurls
     pass
 
 # this is a function where you can manually delete entries from the database as
 # needed you have to manually update the sql script
 def deleteSomething():
-    sql="DELETE FROM lot WHERE name='testlot'"
+    sql="DELETE FROM lot WHERE name='testlot' "
     mycursor.execute(sql)
+    db.commit()
     for i in mycursor:
         print(i)
-
     pass
 
 def main():
     # the functions that are manipulating the database are commented out for now
     # but can be uncommented for testing
     # I will change these once we begin integration (smooshing)
-    
+
+    #createByPasses()
+
     #alterDatabase()
 
-    showDatabase()
+    #setFullness(2, "testlot", 11, 0.0, "testlot.png", "https://www.youtube.com/w\
+atch?v=bhm1Y0pgs-c")
 
-    #setFullness(2, "testlot2", 12, 0.6, "testlot2.dat", "www.parkingoracle.com/testlot2")
+    #setByPasses("Lot E2", "G")
 
-    #updateFullPer("testlot2", 0.5)
+    #updateFullPer("testlot", 0.0)
 
-    #updateSpaces("testlot", 39)
+    #updateSpaces("testlot", 11)
 
-    #updateFilename("testlot2", "Lot23.png")
+    #updateFilename("testlot", "testlot.png")
 
-    imgDict = getImageData("testLot")
-    print(imgDict)
+    #updateUrls("testlot", "https://www.youtube.com/watch?v=bhm1Y0pgs-c")
 
-    spacesTaken = getSpacesTaken("testlot")
-    print(spacesTaken)
+    #imgDict = getImageData("testLot")
+    #print(imgDict)
 
-    names = getNames()
-    print (names)
+    #spacesTaken = getSpacesTaken("testlot")
+    #print(spacesTaken)
+    
+    #names = getNames
+    #print (names)
 
-    urls = getURLs()
-    print (urls)
+    #urls = getURLs()
+    #print (urls)
+
+    showlot()
+
+    showByPasses()
 
     #deleteSomething()
 
 if __name__ == "__main__":
-    sql = "SELECT name, fullPer, spacesTaken from lot"
-    mycursor.execute(sql)
-    Lurls = []
-    Turls = mycursor.fetchall()
-    print(Turls)
-    
+    main()
