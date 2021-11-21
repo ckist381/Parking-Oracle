b26fd66e0a43e242ac8c344670b9c172e3c468be
noir-angel <55473713+noir-angel@users.noreply.github.com>
2021-11-20 21:32:37 -0500

    Add files via upload

--- API/api.js
+++ API/api.js
@@ -10,7 +10,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000; 
 
-app.listen(PORT, () => console.log(`Sever Started on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server Started on port: ${PORT}`));
 
 app.get('/get', async function(req, res) { 
 
